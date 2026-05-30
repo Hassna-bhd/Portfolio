fe709c2ff79174c7fd4d61a9563c9a06fafce1c6
Hassna Bahadou <Hassnaa.bahadou@gmail.com>
2026-05-30 03:09:06 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,86 @@
+function getProjects() {
 
+  return JSON.parse(
+    localStorage.getItem("projects")
+  ) || [];
+
+}
+
+function saveProjects(projects) {
+
+  localStorage.setItem(
+    "projects",
+    JSON.stringify(projects)
+  );
+
+}
+
+function addProject() {
+
+  const title =
+    document.getElementById("title").value;
+
+  const desc =
+    document.getElementById("desc").value;
+
+  const projects =
+    getProjects();
+
+  projects.push({
+    title,
+    desc
+  });
+
+  saveProjects(projects);
+
+  renderProjects();
+
+}
+
+function deleteProject(index) {
+
+  const projects =
+    getProjects();
+
+  projects.splice(index, 1);
+
+  saveProjects(projects);
+
+  renderProjects();
+
+}
+
+function renderProjects() {
+
+  const container =
+    document.getElementById("project-list");
+
+  const projects =
+    getProjects();
+
+  container.innerHTML = "";
+
+  projects.forEach((project, index) => {
+
+    container.innerHTML += `
+      <div class="card mb-3 p-3">
+
+        <h4>${project.title}</h4>
+
+        <p>${project.desc}</p>
+
+        <button
+          class="btn btn-danger"
+          onclick="deleteProject(${index})"
+        >
+          Delete
+        </button>
+
+      </div>
+    `;
+
+  });
+
+}
+
+renderProjects();
